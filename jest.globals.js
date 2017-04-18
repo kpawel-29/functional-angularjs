4d8ad394d4f8a187ab1cee04494069e858e7c5d8
Marcin Sirocki <m.sirocki@madkom.pl>
2017-04-18 15:16:19 +0200

    add globals to jest config

---
+++ jest.globals.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Marcin Sirocki
+ * e-mail: m.sirocki@madkom.pl
+ */
+// window['jQuery'] = require('jquery');
+// window['jquery'] = require('jquery');
+// window['$'] = require('jquery');
